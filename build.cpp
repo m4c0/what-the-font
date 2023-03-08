4fa3aaff897ab241fe92a193ad5bf60581aea02f
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-08 16:10:10 -0300

    freetype/harfbuzz compilation with ecpw

--- build.cpp
+++ build.cpp
@@ -4,5 +4,8 @@ using namespace ecow;
 
 int main(int argc, char **argv) {
   auto all = unit::create<seq>("all");
+  all->add_unit<>("freetype")->add_include_dir("freetype/include");
+  all->add_unit<>("harfbuzz");
+
   return run_main(all, argc, argv);
 }
---
+++ freetype.cpp
@@ -0,0 +1,7 @@
+extern "C" {
+#define FT2_BUILD_LIBRARY
+#include "freetype/src/base/ftbase.c"
+#include "freetype/src/base/ftdebug.c"
+#include "freetype/src/base/ftinit.c"
+#include "freetype/src/base/ftsystem.c"
+}

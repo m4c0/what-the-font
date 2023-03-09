dd73a99ce72d63ee0e42284e70614adf3e09dbbc
Eduardo Costa <m4c0@users.noreply.github.com>
2023-03-09 08:59:14 -0300

    fix build on wasm

--- freetype.cpp
+++ freetype.cpp
@@ -1,4 +1,12 @@
 extern "C" {
+#ifdef __wasm__
+struct jmp_buf {
+  int x;
+};
+void abort();
+void longjmp(jmp_buf env, int val) { abort(); }
+#endif
+
 #define FT_CONFIG_MODULES_H "../../../ftmodule.h"
 #define FT2_BUILD_LIBRARY
 #include "freetype/src/base/ftbase.c"
--- harfbuzz.cpp
+++ harfbuzz.cpp
@@ -1,1 +1,2 @@
+#define HB_NO_MT
 #include "harfbuzz/src/harfbuzz.cc"
